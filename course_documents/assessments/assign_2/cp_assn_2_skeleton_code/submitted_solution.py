689b41c5d2172658b0407a8583eb98aaecee9927
InnesGarmany <2661715G@student.gla.ac.uk>
2024-11-09 23:04:10 +0000

    ILP working for basic example

--- course_documents/assessments/assign_2/cp_assn_2_skeleton_code/submitted_solution.py
+++ course_documents/assessments/assign_2/cp_assn_2_skeleton_code/submitted_solution.py
@@ -19,90 +19,86 @@ import pulp
 # useful for your pipeline)
 def run_ilp(instance_graph, start_node = 1, timeout=1000):
     
-  time_steps = 5  # Number of time steps
-  N = 4  # Number of nodes
-  from_list = [0, 1, 2, 3] 
-  to_list   = [1, 2, 3, 0]
-  initially_on_fire = [0] 
+  num_nodes = 4
+  time_steps = num_nodes + 1
+  from_list = [0, 1, 2, 3]
+  to_list = [1, 2, 3, 0]
+  initially_on_fire = [0]
 
+  firefighter = pulp.LpVariable.dicts("firefighter", ((time, node) for time in range(time_steps) for node in range(num_nodes)), cat="Binary")
+  on_fire = pulp.LpVariable.dicts("on_fire", ((time, node) for time in range(time_steps) for node in range(num_nodes)), cat="Binary")
+
+  # Objective: Minimize the number of nodes on fire at the final time step
   model = pulp.LpProblem("Firefighter_Problem", pulp.LpMinimize)
-
-  firefighter = pulp.LpVariable.dicts("firefighter", ((time, node) for time in range(time_steps) for node in range(N)), cat="Binary")
-  on_fire = pulp.LpVariable.dicts("on_fire", ((time, node) for time in range(time_steps) for node in range(N)), cat="Binary")
-
-  # Objective: Minimize the number of nodes on fire
-  model += pulp.lpSum(on_fire[time_steps-1, node] for node in range(N))
+  model += pulp.lpSum(on_fire[time_steps-1, node] for node in range(num_nodes))
+  
+  #Pre-compute list of neighbors of each node
+  neighbor_dict = {}
+  for node in range(num_nodes):
+    list_of_neighbors = []
+    
+    for i in range(len(from_list)):
+      if from_list[i] == node:
+        list_of_neighbors.append(to_list[i])
+        
+      elif to_list[i] == node:
+        list_of_neighbors.append(from_list[i])
+        
+    neighbor_dict[node] = list_of_neighbors
+                     
 
   # Constraints
   # Node(s) start on fire
-  for node in range(N):
-      if node in initially_on_fire:
-          model += on_fire[0, node] == 1
-          model += firefighter[0,node] != 1
-      else:
-          model += on_fire[0, node] == 0
+  for node in range(num_nodes):
+    if node in initially_on_fire:
+        model += on_fire[0, node] == 1
+        model += firefighter[0, node] == 0
+    else:
+        model += on_fire[0, node] == 0
 
-  # Only one node can be defended per time step
-  model += pulp.lpSum(firefighter[0, node] for node in range(N)) <= 1
-  
-  for time in range(1,time_steps):
-      model += pulp.lpSum(firefighter[time, node] for node in range(N)) <= 1 + pulp.lpSum(firefighter[time-1, node] for node in range(N))
-      
-  # Time step for spread
+  # One additional node can be defended at each step
+  model += pulp.lpSum(firefighter[0, node] for node in range(num_nodes)) <=1
   for time in range(1, time_steps):
-    for node in range(N):
-          
-      #Nodes cannot stop burning
-      if on_fire[time - 1, node]:
-        model += on_fire[time, node] ==1
+        model += pulp.lpSum(firefighter[time, node] for node in range(num_nodes)) <= pulp.lpSum(firefighter[time, node] for node in range(num_nodes)) + 1
         
-        #Continue skips iteration for current node
-        continue
+  # Constraints for fire spread and defense behavior over time
+  for time in range(1, time_steps):
+    for node in range(num_nodes):
+      # Nodes continue burning once ignited
+      model += on_fire[time, node] >= on_fire[time - 1, node]
+
+      # Nodes continue being defended once protected
+      model += firefighter[time, node] >= firefighter[time - 1, node]
+
+      # Protected nodes do not catch fire
+      model += on_fire[time, node] <= 1 - firefighter[time, node]
+
+      # Nodes on fire cannot be defended
+      model += firefighter[time, node] <= 1 - on_fire[time - 1, node]
+
+      # Check for any burning neighbors and, if found, spread the fire
+      burning_neighbor = pulp.lpSum(on_fire[time - 1, neighbor] for neighbor in neighbor_dict[node])
+      # Normalise sum to be 1 
+      model += on_fire[time, node] >= burning_neighbor/len(burning_neighbor) - firefighter[time-1, node]
+
+  model.solve(pulp.GLPK_CMD())
+  result_dict = {"num_saved": num_nodes-pulp.value(model.objective)}
+  
+  if __name__ == "__main__":
+    for time in range(time_steps):
+      # Defended nodes and their values
+      defended_nodes = [(node, pulp.value(firefighter[time, node])) for node in range(num_nodes) if pulp.value(firefighter[time, node]) is not None]
       
-      #Nodes cannot stop being defended
-      if firefighter[time-1, node]==1:
-        model += firefighter[time, node] == 1
+      # On fire nodes and their values
+      on_fire_nodes = [(node, pulp.value(on_fire[time, node])) for node in range(num_nodes) if pulp.value(on_fire[time, node]) is not None]
       
-      #Defended nodes can't catch fire
-      if firefighter[time, node] == 1:
-        model += on_fire[time, node] == 0
-        continue
-      
-      #Flaming nodes cannot be defended
-      if on_fire[time-1, node] == 1:
-        model += firefighter[time, node] == 0
-        continue
-      
-      
-      # Iterate until burning neighbor found
-      i = 0
-      burning_neighbor = False
-      while not burning_neighbor and i < len(from_list):
-          #Checks if a neighbor was burning at the previous time step
-          if to_list[i] == node:
-              burning_neighbor = (on_fire[time - 1, from_list[i]] == 1)
-          elif from_list[i] == node:
-              burning_neighbor = (on_fire[time - 1, to_list[i]] == 1)
-          i += 1
-
-      # If a burning neighbor was found, the node catches fire
-      model += on_fire[time, node] >= burning_neighbor
-
-
-  # Solve the model
-  model.solve()
-
-  # Print results
-  print("Status:", pulp.LpStatus[model.status])
-  for time in range(time_steps):
-      for node in range(N):
-          if pulp.value(firefighter[time, node]) == 1:
-              print(f"Time {time}: Firefighter protects node {node}")
-          if pulp.value(on_fire[time, node]) == 1:
-              print(f"Time {time}: Node {node} is on fire")
-
-  return {'Finished'}
-
+      print(f"Time {time}:")
+      print("  Defended nodes and values:", defended_nodes)
+      print("  On fire nodes and values:", on_fire_nodes)
+      print("\n")
+    print(neighbor_dict)
+    
+  return result_dict
 
 
 #
@@ -129,8 +125,7 @@ def run_cp(instance_graph, start_node = 1, timeout=1000):
   model = minizinc.Model()
   model.add_file("Firefighter.mzn")
   model.add_file(file_name)
-  
-  
+
   solver = minizinc.Solver.lookup("chuffed")
   instance = minizinc.Instance(solver, model)
   
